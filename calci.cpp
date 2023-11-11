fe7477074b3351bb315c4f3a211dd05278e86051
shaanjijoe <shaanjijoe2@gmail.com>
2023-11-11 12:16:58 +0530

    Subtraction added and main modified

--- calci.cpp
+++ calci.cpp
@@ -1,6 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+int sub(int a, int b)
+{
+    return a-b;
+}
+
 int main()
 {
+    cout<<"Main Function"<<endl;
     return 0;
 }
