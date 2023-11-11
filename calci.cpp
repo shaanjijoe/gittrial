51234f7831bef37dbcdd684bd682c43d947903b6
Harman <harmansaluja02@gmail.com>
2023-11-11 12:19:28 +0530

    calci features added

--- calci.cpp
+++ calci.cpp
@@ -1,5 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+int add (int a,int b)
+{
+    return a+b;
+}
+int multi(int a,int b)
+{
+    return a*b;
+}
+int divi(int a,int b)
+{
+    return a/b;
+}
 int main()
 {
     return 0;
